4e19a9f92c41d8accc14013eb717a6d0c16edb6c
joaopedrohagen <joaopedro@hagen.dev.br>
2024-05-17 09:39:21 -0300

    :globe_with_meridians: feat: create route table

---
+++ igw.tf
@@ -0,0 +1,26 @@
+resource "aws_internet_gateway" "monitoramento_igw" {
+  vpc_id = aws_vpc.monitoramento_vpc.id
+
+  tags = merge(
+    local.tags,
+    {
+      Name = "${var.project_name}-igw"
+    }
+  )
+}
+
+resource "aws_route_table" "monitoramento_pub_rtb" {
+  vpc_id = aws_vpc.monitoramento_vpc.id
+
+  route {
+    cidr_block = var.all_ipv4
+    gateway_id = aws_internet_gateway.monitoramento_igw.id
+  }
+
+  tags = merge(
+    local.tags,
+    {
+      Name = "${var.project_name}-pub-rtb"
+    }
+  )
+}
--- variables.tf
+++ variables.tf
@@ -7,3 +7,13 @@ variable "project_name" {
   type        = string
   description = "Project name variable"
 }
+
+variable "all_ipv4" {
+  type        = string
+  description = "Releases traffic for all IPV4"
+}
+
+variable "all_ipv6" {
+  type        = string
+  description = "Releases traffic for all IPV6"
+}
