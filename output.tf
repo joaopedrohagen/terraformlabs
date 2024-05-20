187774c95434de849ddab5f6605daad4dedf7fdb
joaopedrohagen <joaopedro@hagen.dev.br>
2024-05-20 09:02:20 -0300

    :arrow_down: feat: define output modules

---
+++ output.tf
@@ -0,0 +1,15 @@
+output "subnet_pub_1a" {
+  value = module.monitoramento_network.subnet_pub_1a
+}
+
+output "subnet_pub_1b" {
+  value = module.monitoramento_network.subnet_pub_1b
+}
+
+output "subnet_priv_1a" {
+  value = module.monitoramento_network.subnet_priv_1a
+}
+
+output "subnet_priv_1b" {
+  value = module.monitoramento_network.subnet_priv_1b
+}
